4692490a9e08d3811ccf68668fc6991a94cc713a
Дмитрий <dimmich2001@gmail.com>
2020-03-09 21:24:50 +0200

    updated exception task by removing unnecessary variable checking

--- core/src/main/java/com/alevel/java/nix/exception/ArithmeticProgression.java
+++ core/src/main/java/com/alevel/java/nix/exception/ArithmeticProgression.java
@@ -17,8 +17,6 @@ public class ArithmeticProgression {
     public int calculate(int n) throws ProgressionConfigurationException {
         if (n <= 0) {
             throw new ProgressionConfigurationException("Number of element of progress can't be negative or 0");
-        } else if (step == 0) {
-            throw new ProgressionConfigurationException("Step of progression can't be 0");
         } else {
             return (initial + (n - 1) * step);
         }
