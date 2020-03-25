b532527f81824596c3e3c4845f53885a445a8f2d
Дмитрий <dimmich2001@gmail.com>
2020-03-25 15:54:24 +0200

    updated longest duration task to O(n) complexity

--- core/src/main/java/com/alevel/java/nix/data/duration/LongestDuration.java
+++ core/src/main/java/com/alevel/java/nix/data/duration/LongestDuration.java
@@ -9,6 +9,18 @@ public class LongestDuration {
 
     public Duration getLongestTime(List<LocalDateTime> dataList) {
 
+        var max = dataList.get(0);
+        var min = dataList.get(1);
+
+        for (LocalDateTime element : dataList) {
+            if (element.compareTo(max) > 0) {
+                max = element;
+            }
+            if (element.compareTo(min) < 0) {
+                min = element;
+            }
+        }
+
         Arrays.sort(dataList.toArray());
 
         return Duration.between(dataList.get(0), dataList.get(dataList.size() - 1));
