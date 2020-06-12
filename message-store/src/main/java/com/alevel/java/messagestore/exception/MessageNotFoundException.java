0bbc3483bb5dedbf843b042b8492d789fabdb612
Дмитрий <dimmich2001@gmail.com>
2020-06-12 18:46:19 +0300

    added new spring module that includes message-store task

---
+++ message-store/src/main/java/com/alevel/java/messagestore/exception/MessageNotFoundException.java
@@ -0,0 +1,15 @@
+package com.alevel.java.messagestore.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.UUID;
+
+public class MessageNotFoundException extends ResponseStatusException {
+
+    public MessageNotFoundException(UUID id) {
+        super(HttpStatus.NOT_FOUND, "message with id " + id + " was not found");
+    }
+
+}
---
+++ message-store/src/main/java/com/alevel/java/messagestore/repository/MessageRepository.java
@@ -0,0 +1,9 @@
+package com.alevel.java.messagestore.repository;
+
+import com.alevel.java.messagestore.entity.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface MessageRepository extends JpaRepository<Message, UUID> {
+}
